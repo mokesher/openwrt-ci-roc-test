1e90e25a4ec18c0dd390377893e72170290bb557
Moke <Moke@github.cn>
2026-03-19 14:25:25 +0800

    add small8

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -1,3 +1,46 @@
+# 安装feeds
+BUILD_DIR="./"
+FEEDS_CONF="feeds.conf.default"
+
+update_feeds() {
+    local FEEDS_PATH="$BUILD_DIR/$FEEDS_CONF"
+    if [[ -f "$BUILD_DIR/feeds.conf" ]]; then
+        FEEDS_PATH="$BUILD_DIR/feeds.conf"
+    fi
+    sed -i '/^#/d' "$FEEDS_PATH"
+    sed -i '/packages_ext/d' "$FEEDS_PATH"
+
+    if ! grep -q "small-package" "$FEEDS_PATH"; then
+        [ -z "$(tail -c 1 "$FEEDS_PATH")" ] || echo "" >>"$FEEDS_PATH"
+        echo "src-git small8 https://github.com/kenzok8/jell" >>"$FEEDS_PATH"
+    fi
+
+#    if ! grep -q "openwrt-passwall" "$FEEDS_PATH"; then
+#        [ -z "$(tail -c 1 "$FEEDS_PATH")" ] || echo "" >>"$FEEDS_PATH"
+#        echo "src-git passwall https://github.com/Openwrt-Passwall/openwrt-passwall;main" >>"$FEEDS_PATH"
+#    fi
+#
+#    if ! grep -q "openwrt_bandix" "$BUILD_DIR/$FEEDS_CONF"; then
+#        [ -z "$(tail -c 1 "$BUILD_DIR/$FEEDS_CONF")" ] || echo "" >>"$BUILD_DIR/$FEEDS_CONF"
+#        echo 'src-git openwrt_bandix https://github.com/timsaya/openwrt-bandix.git;main' >>"$BUILD_DIR/$FEEDS_CONF"
+#    fi
+
+#    if ! grep -q "luci_app_bandix" "$BUILD_DIR/$FEEDS_CONF"; then
+#        [ -z "$(tail -c 1 "$BUILD_DIR/$FEEDS_CONF")" ] || echo "" >>"$BUILD_DIR/$FEEDS_CONF"
+#        echo 'src-git luci_app_bandix https://github.com/timsaya/luci-app-bandix.git;main' >>"$BUILD_DIR/$FEEDS_CONF"
+#    fi
+#
+    if [ ! -f "$BUILD_DIR/include/bpf.mk" ]; then
+        touch "$BUILD_DIR/include/bpf.mk"
+    fi
+
+    ./scripts/feeds update -a
+}
+
+update_feeds
+
+
+
 # 修改默认IP & 固件名称 & 编译署名和时间
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 sed -i "s/hostname='.*'/hostname='Roc'/g" package/base-files/files/bin/config_generate
@@ -94,5 +137,19 @@ git clone --depth=1 https://github.com/vernesong/OpenClash package/luci-app-open
 # 清理 PassWall 的 chnlist 规则文件
 echo "baidu.com"  > package/luci-app-passwall/luci-app-passwall/root/usr/share/passwall/rules/chnlist
 
-./scripts/feeds update -a
-./scripts/feeds install -a
+#./scripts/feeds update -a
+#./scripts/feeds install -a
+
+./scripts/feeds update -i
+for dir in $BUILD_DIR/feeds/*; do
+    if [ -d "$dir" ] && [[ ! "$dir" == *.tmp ]] && [[ ! "$dir" == *.index ]] && [[ ! "$dir" == *.targetindex ]]; then
+        if [[ $(basename "$dir") == "small8" ]]; then
+            ./scripts/feeds install -p small8 -f taskd luci-lib-taskd luci-app-store quickstart \
+        luci-app-quickstart luci-app-istorex
+#        elif [[ $(basename "$dir") == "passwall" ]]; then
+#            install_passwall
+        else
+            ./scripts/feeds install -f -ap $(basename "$dir")
+        fi
+    fi
+done
