bd8552cd3bb1a545166d8f4a7284d198156587aa
moke <moke@gmail.cn>
2026-09-22 10:33:45 +0800

    fix tailscale version

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -342,8 +342,11 @@ fi
 git clone --depth=1 https://github.com/asvow/luci-app-tailscale feeds/packages/luci-app-tailscale
 git clone --depth=1 https://github.com/mokesher/luci-app-dashboard feeds/packages/luci-app-dashboard
 
-rm -rf feeds/packages/net/tailscale
-git clone --depth=1 https://github.com/mokesher/tailscale feeds/packages/net/tailscale
+#rm -rf feeds/packages/net/tailscale
+#git clone --depth=1 https://github.com/mokesher/tailscale feeds/packages/net/tailscale
+
+git_sparse_clone master https://github.com/laipeng668/packages net/tailscale
+mv package/tailscale feeds/packages/net/tailscale
 
 tailscale_path="feeds/packages/luci-app-tailscale/root/usr/share/luci/menu.d/luci-app-tailscale.json"
 if [ -f "$tailscale_path" ]; then
